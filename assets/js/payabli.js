bbeb4449e59b5956266a895dbfb4342e7a77e2e4
Francisco Montilla <ciscomontilla@gmail.com>
2024-09-06 01:59:19 -0400

    fix

--- assets/js/payabli.js
+++ assets/js/payabli.js
@@ -39,15 +39,15 @@ var expressCheckoutConfig = {
     console.log("Express Checkout ready: ", data);
     const container = document.querySelector("#express-checkout");
     const separator = document.querySelector("#separator-checkout");
-    container.classList.remove("hidden")
-    separator.classList.remove("hidden")
+    container.classList.remove("hidden");
+    separator.classList.remove("hidden");
   },
   functionCallBackSuccess: function (data) {
     console.log("Express Checkout Success: ", data);
     const container = document.querySelector("#container-payment");
     const purchaseSuccess = document.querySelector("#purchase-success");
-    container.classList.add("hidden")
-    purchaseSuccess.classList.remove("hidden")
+    container.classList.add("hidden");
+    purchaseSuccess.classList.remove("hidden");
   },
   functionCallBackError: function (data) {
     console.log("Express Checkout Error: ", data);
@@ -58,102 +58,105 @@ var expressCheckoutConfig = {
 };
 const component = new PayabliComponent(expressCheckoutConfig);
 
-  // Card Checkout config object
-  var cardCheckoutConfig = {
-    type: "methodEmbedded",
-    rootContainer: "pay-component-card",
-    defaultOpen: "card",
-    token: token,
-    entryPoint: entryPoint,
-    card: {
-      enabled: true,
-      amex: false,
-      discover: true,
-      visa: true,
-      mastercard: true,
-      inputs: {
-        //here we are customizing the input fields
-        cardHolderName: {
-          label: "Card Holder Name",
-          placeholder: "",
-          floating: false,
-          value: "John Doe",
-          size: 12,
-          row: 0,
-          order: 0,
-        },
-        cardNumber: {
-          label: "Card number",
-          placeholder: "1234 1234 1234 1234",
-          floating: false,
-          size: 6,
-          row: 1,
-          order: 0,
-        },
-        cardExpirationDate: {
-          label: "Exp Date",
-          placeholder: "MM/YY",
-          floating: false,
-          size: 6,
-          row: 1,
-          order: 1,
-        },
-        cardCvv: {
-          label: "CVV/CVC",
-          placeholder: "CVV/CVC",
-          floating: false,
-          size: 6,
-          row: 2,
-          order: 0,
-        },
-        cardZipcode: {
-          label: "Zip Code",
-          placeholder: "Zip/Postal Code",
-          floating: false,
-          size: 6,
-          row: 2,
-          order: 1,
-        },
+// Card Checkout config object
+var cardCheckoutConfig = {
+  type: "methodEmbedded",
+  rootContainer: "pay-component-card",
+  defaultOpen: "card",
+  token: token,
+  entryPoint: entryPoint,
+  card: {
+    enabled: true,
+    amex: false,
+    discover: true,
+    visa: true,
+    mastercard: true,
+    inputs: {
+      //here we are customizing the input fields
+      cardHolderName: {
+        label: "Card Holder Name",
+        placeholder: "",
+        floating: false,
+        value: "John Doe",
+        size: 12,
+        row: 0,
+        order: 0,
+      },
+      cardNumber: {
+        label: "Card number",
+        placeholder: "1234 1234 1234 1234",
+        floating: false,
+        size: 6,
+        row: 1,
+        order: 0,
+      },
+      cardExpirationDate: {
+        label: "Exp Date",
+        placeholder: "MM/YY",
+        floating: false,
+        size: 6,
+        row: 1,
+        order: 1,
+      },
+      cardCvv: {
+        label: "CVV/CVC",
+        placeholder: "CVV/CVC",
+        floating: false,
+        size: 6,
+        row: 2,
+        order: 0,
+      },
+      cardZipcode: {
+        label: "Zip Code",
+        placeholder: "Zip/Postal Code",
+        floating: false,
+        size: 6,
+        row: 2,
+        order: 1,
       },
     },
-    ach: {
-      enabled: false,
-    },
-    customerData: {
-      customerNumber: "00001",
-      firstName: "John",
-      lastName: "Doeee",
-      billingEmail: "johndoe@email.com",
-    },
-    functionCallBackSuccess: function (response) {
-      alert("card success");
-      // paycomponentCard.removeListener();
-      paycomponentCard.payabliExec("reinit");
-    },
-    functionCallBackReady: function (data) {
-        const container = document.querySelector("#pay-component-card");
-        container.classList.remove("hidden")
-    },
-    functionCallBackError: function (errors) {
-      alert("Error!");
-      console.log(errors);
-    },
-  };
+  },
+  ach: {
+    enabled: false,
+  },
+  customerData: {
+    customerNumber: "00001",
+    firstName: "John",
+    lastName: "Doeee",
+    billingEmail: "johndoe@email.com",
+  },
+  functionCallBackSuccess: function (response) {
+    // paycomponentCard.removeListener();
+    const container = document.querySelector("#container-payment");
+    const purchaseSuccess = document.querySelector("#purchase-success");
+    container.classList.add("hidden");
+    purchaseSuccess.classList.remove("hidden");
+    paycomponentCard.payabliExec("reinit");
+  },
+  functionCallBackReady: function (data) {
+    const container = document.querySelector("#pay-component-card");
+    container.classList.remove("hidden");
+  },
+  functionCallBackError: function (errors) {
+    alert("Error!");
+    console.log(errors);
+  },
+};
 
-  const paycomponentCard = new PayabliComponent(cardCheckoutConfig);
+const paycomponentCard = new PayabliComponent(cardCheckoutConfig);
 
-  const makePayment = (amount) => {
-    paycomponentCard.payabliExec("pay", {
-        paymentDetails: {
-          totalAmount: amount,
-          serviceFee: 2,
-          categories: [
-            {
-              label: "payment",
-              amount: amount,
-              qty: 1
-            }
-          ]
-        }
-      });
-  }
+const makePayment = (amount) => {
+  paycomponentCard.payabliExec("pay", {
+    paymentDetails: {
+      totalAmount: amount,
+      serviceFee: 2,
+      categories: [
+        {
+          label: "payment",
+          amount: amount,
+          qty: 1,
+        },
+      ],
+    },
+  });
+};
